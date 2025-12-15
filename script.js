2f5de3cd1ff687e0ed108e94e4ef61226c47c9bd
EliMarago <elisabetta998@hotmail.it>
2025-12-15 19:36:30 +0100

    add new

--- script.js
+++ script.js
@@ -68,13 +68,27 @@ if (btnNav && header) {
 
 // Chiudi cliccando sui link
 document.querySelectorAll(".main-nav-link").forEach(link => {
-  link.addEventListener("click", () => {
-    if (header) {
+  link.addEventListener("click", (e) => {
+    const href = link.getAttribute("href");
+
+    // se è un anchor interno (#)
+    if (href && href.startsWith("#")) {
+      e.preventDefault();
+
+      // chiudi menu
       header.classList.remove("nav-open");
       body.style.overflow = "auto";
+
+      // aspetta un frame, poi scrolla
+      setTimeout(() => {
+        document.querySelector(href)?.scrollIntoView({
+          behavior: "smooth"
+        });
+      }, 300);
     }
   });
 });
+
 
 // Chiudi cliccando sull'overlay
 if (navOverlay && header) {
